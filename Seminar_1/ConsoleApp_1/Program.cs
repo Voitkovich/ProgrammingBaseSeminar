c6ed65535eb6e0b91a92bc5655d6ce3f040c5286
Ekaterina Voitkovich <styardess@mail.ru>
2022-10-14 13:10:36 +0300

    ConsoleApp

---
+++ Seminar_1/ConsoleApp_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая принимает на вход целое число и выдает его квадрат.
+
+// Запрос от пользователя
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+// Квадрат исходного числа
+int sqrNumber = number * number;
+
+// Вывод результата на экран
+Console.WriteLine($"Квадрат вашего числа = {sqrNumber}");
+
